03cd516f665a27d9b11324c284bc55473cc7cb02
mofizur <mohammedeiman0177@gmail.com>
2023-10-07 15:58:05 +0600

    New items added

---
+++ Basics/oddEven.js
@@ -0,0 +1,11 @@
+function odd_even(num) {
+  if (num % 2 == 0) {
+    return "EVEN";
+  } else {
+    return "ODD";
+  }
+}
+
+console.log(odd_even(0));
+console.log(odd_even(6));
+console.log(odd_even(3));
