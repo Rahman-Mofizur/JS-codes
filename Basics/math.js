4385042174009ccace4ab746355843f91a4f2248
mofizur <mohammedeiman0177@gmail.com>
2023-10-13 22:11:57 +0600

    Maths

---
+++ Basics/math.js
@@ -0,0 +1,12 @@
+var number1 = 25.456;
+var number2 = 35.999;
+
+console.log(number1);
+console.log(Math.round(number1));
+console.log(Math.round(number2));
+
+console.log(Math.abs(-55)); // Absolute number
+
+console.log(Math.pow(2, 3)); // 2^3
+
+console.log(Math.PI); // Pi এর মান show করবে।
